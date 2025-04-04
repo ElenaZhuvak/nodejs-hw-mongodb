d5f832a34680530d0291e5a6fb4991b5fbeecfd7
Elena Zhuvak <elenkazh@gmail.com>
2025-04-04 20:34:44 +0100

    adds function getEnvVar using dotenv

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import { getEnvVar } from './utils/getEnvVar.js';
 
-const PORT = 3000;
-export const setupServer = () => {
+const PORT = Number(getEnvVar('PORT', 3000));
+export const setupServer = () => {  
     const app = express();
     app.use(express.json());
     app.use(cors());
@@ -13,11 +14,11 @@ export const setupServer = () => {
         },
     }));
 
-    app.get('/', (req, res) => {
-        res.json({
-            message: 'Hello'
-        });
-    });
+    // app.get('/', (req, res) => {
+    //     res.json({
+    //         message: 'Hello'
+    //     });
+    // });
     
     app.use('*', (req, res, next) => {
         res.status(404).json({
