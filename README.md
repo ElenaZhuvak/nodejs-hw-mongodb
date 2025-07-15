7061e44dec1fcc5650d7c63e99f5bf984a57da79
Elena Zhuvak <elenkazh@gmail.com>
2025-07-15 22:27:55 +0100

    adds readme file

---
+++ README.md
@@ -0,0 +1,10 @@
+Updates in Homework #4 (branch hw4-validation)
+In this homework, the following changes were made:
+
+1. Updated Express to version 5: The project has been upgraded to the latest version of the Express framework, allowing us to utilize new features and improvements.
+
+2. Removal of the ctrlWrapper function: With the upgrade to Express 5, error handling has become more streamlined, making the ctrlWrapper function unnecessary. As a result, this wrapper has been removed.
+
+3. Changes in Routes: Consequently, all routes have been updated to remove the ctrlWrapper, resulting in cleaner and more maintainable code.
+
+These updates simplify the error-handling logic and transition the project to a more modern framework version.
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import express from 'express';
 import { createContactController, deleteContactController, getAllContactsController, getContactByIdController, replaceContactController, updateContactController,  } from "../controllers/contacts.js";
-import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { validateId } from "../middlewares/validateId.js";
@@ -9,14 +8,14 @@ import { validateId } from "../middlewares/validateId.js";
 export const contactsRouter = Router();
 const jsonParser = express.json();
 
-contactsRouter.get('/', ctrlWrapper(getAllContactsController));
+contactsRouter.get('/', getAllContactsController);
 
-contactsRouter.get('/:contactId', validateId, ctrlWrapper(getContactByIdController));
+contactsRouter.get('/:contactId', validateId, getContactByIdController);
 
-contactsRouter.post('/', jsonParser, validateBody(createContactSchema), ctrlWrapper(createContactController));
+contactsRouter.post('/', jsonParser, validateBody(createContactSchema), createContactController);
 
-contactsRouter.delete('/:contactId', validateId, ctrlWrapper(deleteContactController));
+contactsRouter.delete('/:contactId', validateId, deleteContactController);
 
-contactsRouter.put('/:contactId', validateId, jsonParser, validateBody(createContactSchema), ctrlWrapper(replaceContactController));
+contactsRouter.put('/:contactId', validateId, jsonParser, validateBody(createContactSchema), replaceContactController);
 
-contactsRouter.patch('/:contactId', validateId, jsonParser, validateBody(updateContactSchema), ctrlWrapper(updateContactController));
+contactsRouter.patch('/:contactId', validateId, jsonParser, validateBody(updateContactSchema), updateContactController);
